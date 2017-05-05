e893ce4492162fa04c570d2e72c7689ed77b5853
yangjiadong <yangjiadong@ycode.cn>
2017-05-05 15:45:09 +0800

    'init'

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@
  */
 
 module.exports={
-    database:"mongodb://localhost:27017/test",
+    database:process.env.db+"/domain"||"mongodb://localhost:27017/test",
     secret:"secret_jiadongyang",
-    port:"8080"
+    port:"80"
 };
